f27daacefccba8cf6ddc5611f00497f29767d4e6
omkarpadhye9661 <93431062+omkarpadhye9661@users.noreply.github.com>
2022-02-19 18:30:25 +0530

    Add files via upload

---
+++ TablesController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class TablesController : Controller
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = Products; Integrated Security = True; ")
+        SqlCommand cmd = new SqlCommand();
+
+
+        // GET: Tables
+        public ActionResult Index()
+        {
+            Tables.CategoryId = Categories.CategoryId;
+            conn.Open();
+            cmd.Connection = conn;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "select * from table,Categories where Product.CategoryId =Categories.CategoryId ";
+            List<ProductCategory> plist = new List<ProductCategory>();
+            try
+            {
+
+                SqlDataReader dr = new SqlDataReader();
+                while (dr.Read())
+                {
+                    plist.Add(new ProductCategory
+                    {
+                        ProductId = (int)dr["ProductId"],
+                        ProductName = (string)dr["ProductName"],
+                        Rate = (decimal)dr[Rate],
+                        Discription = (string)dr[Discription],
+                        CategoryName = (string)dr[CategoryName]
+                    })
+                }
+                dr.Close();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.err = ex.Message;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return View(plist);
+            }
+        }
+
+        // GET: Tables/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // GET: Tables/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tables/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductId,ProductName,Rate,Description,CategoryName")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tables.Add(table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table);
+        }
+
+        // GET: Tables/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Tables/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Rate,Description,CategoryName")] Table table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table);
+        }
+
+        // GET: Tables/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table table = db.Tables.Find(id);
+            if (table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table);
+        }
+
+        // POST: Tables/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table table = db.Tables.Find(id);
+            db.Tables.Remove(table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
